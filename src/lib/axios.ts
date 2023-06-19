6c1cfd194beade112f7b1a4ab14cb757b67b3850
muneebtech <ranamuneebtahir@gmail.com>
2023-06-20 01:39:09 +0500

    fix authorization

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,10 +1,14 @@
+import { decryptData } from "@/utils/localStorage";
 import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from "axios";
 
 const apiEndpoint: string = process.env.NEXT_PUBLIC_API_ENDPOINT as string;
-
+const authToken = decryptData("token")
 const client = axios.create({
   baseURL: apiEndpoint,
-  withCredentials: true,
+  // withCredentials: true,
+  headers: {
+    Authorization: `Bearer ${authToken}`, // Add the bearer token to the Authorization header
+  },
 });
 
 export async function request(options: AxiosRequestConfig) {
