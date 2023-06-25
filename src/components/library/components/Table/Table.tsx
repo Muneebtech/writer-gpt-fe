97c08d677b66af4f95e4e142584be05b0c716506
muneebtech <ranamuneebtahir@gmail.com>
2023-06-25 17:50:19 +0500

    gpt logs and script generation

--- src/components/library/components/Table/Table.tsx
+++ src/components/library/components/Table/Table.tsx
@@ -69,14 +69,14 @@ const Table = () => {
               <table className="min-w-full divide-y divide-gray-300 text-sm">
                 <thead>
                   <tr className="">
-                    <th className="py-4 px-4 text-center">Video Name</th>
-                    <th className="py-4 px-4 text-center">Language</th>
-                    <th className="py-4 px-4 text-center">Video Topic</th>
-                    <th className="py-4 px-4 text-center">Outro</th>
+                    <th className="py-4 px-4 text-left">Video Name</th>
+                    <th className="py-4 px-4 text-left">Language</th>
+                    <th className="py-4 px-4 text-left">Video Topic</th>
+                    <th className="py-4 px-4 text-left">Outro</th>
                     <th className="py-4 px-4 text-center">GPT Logs</th>
-                    <th className="py-4 px-4 text-center">Word Count</th>
+                    <th className="py-4 px-4 text-left">Word Count</th>
                     <th className="py-4 px-4 text-center">Script</th>
-                    <th className="py-4 px-4 text-center">Voiceover</th>
+                    {/* <th className="py-4 px-4 text-center">Voiceover</th> */}
                     {/* <th className="py-4 px-4 text-center">Date</th> */}
                   </tr>
                 </thead>
@@ -100,7 +100,7 @@ const Table = () => {
                           index !== Data.length - 1 ? "table-bb-gray" : ""
                         }`}
                       >
-                        <td className="py-4 px-4 text-center ">
+                        <td className="py-4 px-4 text-left ">
                           <div className="flex items-center">
                             <Image
                               loader={() => src}
@@ -114,13 +114,13 @@ const Table = () => {
                           </div>
                         </td>
 
-                        <td className="py-4 px-4 text-center">
+                        <td className="py-4 px-4 text-left">
                           {row?.model?.model}
                         </td>
-                        <td className="py-4 px-4 text-center">
+                        <td className="py-4 px-4 text-left">
                           {row?.topic?.topic}
                         </td>
-                        <td className="py-4 px-4 text-center">
+                        <td className="py-4 px-4 text-left">
                           {row?.outro?.outro}
                         </td>
                         <td className="py-4 px-4">
@@ -136,9 +136,9 @@ const Table = () => {
                             </Button>
                           </div>
                         </td>
-                        <td className="py-4 px-4 text-center">
+                        <td className="py-4 px-4 text-left">
                           <span
-                            className=" text-center rounded-3xl py-2 px-6"
+                            className=" text-left rounded-3xl py-2 px-6"
                             style={{
                               backgroundColor: `${backgroundColor}`,
                             }}
@@ -146,7 +146,7 @@ const Table = () => {
                             {row.wordCount}
                           </span>
                         </td>
-                        <td className="py-4 px-4">
+                        <td className="py-4 px-4 text-left">
                           <div className="flex justify-center">
                             <Button
                               disabled={row?.script ? false : true}
@@ -159,7 +159,7 @@ const Table = () => {
                             </Button>
                           </div>
                         </td>
-                        <td className="py-4 px-4">
+                        {/* <td className="py-4 px-4">
                           <div className="flex justify-center">
                             <Button
                               disabled={true}
@@ -171,7 +171,7 @@ const Table = () => {
                               <FiDownload />
                             </Button>
                           </div>
-                        </td>
+                        </td> */}
                         {/* <td className="py-4 px-4 text-center">{row.date}</td> */}
                       </tr>
                     );
